3d336c484346535dd3ab932f1da60ab89500c2d5
llturtle22ll <angelopazin@gmail.com>
2024-05-21 16:36:30 -0300

    Aaaa

--- lamport_clock.py
+++ lamport_clock.py
@@ -8,5 +8,6 @@ rank = comm.Get_rank()
 if rank == 0:
     data = {'a': 7, 'b': 3.14}
     comm.send(data, dest=1, tag=11)
+    print("entrei")
 elif rank == 1:
     data = comm.recv(source=0, tag=11)
